125fb2a47481f83f8e0f81f2ebf7dd26a9c2ae13
dyxling <42081380+dyxling@users.noreply.github.com>
2018-08-04 11:03:39 +0800

    dropdown menu

---
+++ pages/index/index.js
@@ -0,0 +1,35 @@
+var nav=require("./menu.js");//导入菜单
+var imgs = require("./imgs.js");//导入图片
+const app=getApp();
+Page({
+  data: {
+    menu:nav,
+    mvalue: [nav[0].level1, nav[1].level1,nav[2].level1,nav[3].level1],
+    pos:[0,0,0,0],
+    img: ["../../img/1.jpg", "../../img/1.jpg", "../../img/1.jpg"],
+    clicked:999
+  },
+  onLoad:function(options){
+    const that=this;
+    
+  },
+  togg:function(event){
+    const that=this;
+    let i = event.currentTarget.dataset.choosen
+    let ix=event.target.dataset.index
+    let id=event.target.dataset.id
+    that.setData({ clicked: event.currentTarget.dataset.choosen});
+    that.data.pos[i]=ix
+    if(i==id){
+      that.data.mvalue[i] = event.target.dataset.text
+      that.setData({mvalue:that.data.mvalue})
+      let t = that.data.pos;
+      t=""+t[0]+t[1]+t[2]+t[3]
+      console.log(t)
+      if(i==3){
+        that.data.img=imgs[t]
+        that.setData({img:that.data.img})
+      }
+    }
+  }
+})
